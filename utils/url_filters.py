b9c54b1a43c00bf5d37d03cf0e5ba633b556dab6
Zackery He <zackeryhe03@gmail.com>
2026-02-07 13:01:14 -0800

    data filter

--- utils/url_filters.py
+++ utils/url_filters.py
@@ -6,7 +6,7 @@ from utils.tokenizer import tokenize_string
 
 EXACT_TRAP_PARAMS = {'q', 's'}
 CONTAINS_TRAP_PARAMS = {'search', 'query', 'filter', 'share'}
-DATASET_PATH_SEGMENTS = {'sampledata', 'dataset', 'datasets', 'rawdata'}
+DATASET_PATH_SEGMENTS = {'sampledata', 'dataset', 'datasets', 'rawdata', 'raw-attachment'}
 MAX_PAGINATION_PAGE = 5
 PAGINATION_QUERY_PARAMS = ['p=', 'cat=', 'author=', 'page_id=', 'limit=']
 
